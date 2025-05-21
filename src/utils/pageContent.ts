fd3afc46bda8a103b7540bd549571781aaec4c85
urielaweobe <urielaweobe@gmail.com>
2025-05-22 00:05:13 +0100

    update button text in PATRON_CONTENT and ARTISAN_CONTENT for consistency

--- src/utils/pageContent.ts
+++ src/utils/pageContent.ts
@@ -4,7 +4,7 @@ export const PATRON_CONTENT: ContentConfig = {
   title: "Need an Artisan?\nWe know a guy",
   description:
     "Join the waitlist and be the first to know when we launch in your area!",
-  buttonText: "Join the waitlist",
+  buttonText: "Get early access",
   marqueeText:
     "100+ VETTED ARTISANS  QUALITY YOU CAN TRUST  WALLET FRIENDLY PRICES",
   formLink: "/form",
@@ -15,7 +15,7 @@ export const ARTISAN_CONTENT: ContentConfig = {
   title: "Looking for customers?\nWe go run am",
   description:
     "Join the waitlist and be the first to know when we launch in your area!",
-  buttonText: "Join the waitlist",
+  buttonText: "Get early access",
   marqueeText: "INCREASE YOUR REVENUE  GROW YOUR CUSTOMER BASE",
   formLink: "/form",
   name: "artisan",
