b727e612c8fca9b4007bb7d209b7aef8dbcfce2b
SerikiAyodele <serikiayodelee@gmail.com>
2026-02-09 06:38:40 +0100

    update text

--- src/app/patrons/form/page.tsx
+++ src/app/patrons/form/page.tsx
@@ -9,10 +9,9 @@ const PatronFormPage = () => {
       title={
         <>
           Need an Artisan? <br />
-          We know a guy
         </>
       }
-      description="Need a verified artisan? Fill out this form, and we’ll contact you on WhatsApp to confirm details. We might also ask for photos or a quick video of the job."
+      description="Fill out this form, and we’ll contact you on WhatsApp to confirm details. We might also ask for photos or a quick video of the job."
       formComponent={<PatronForm />}
       selectedForm="patron"
     />
